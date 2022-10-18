0e82a9cb5d58b97326b560134a670d05e2aca64f
chunni <guok.zhao.life@outlook.com>
2022-10-18 16:17:50 +0800

    完成 ex1，编写 .gitignore

---
+++ ex1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+#\File        : ex1.py
+#\Author      : chunni
+#\Email       : shachi1758@outlook.com
+#\CreateTime  : 2022-10-18 15:55:26
+#\Version     : 0.1.0
+#\Brief       : 第一个程序
+#\Log         :
+#             : 巩固练习：了解注释 # 的作用
+
+print("Hello World!")
+# print("Hello Again!")
+# print("I like typing this.")
+# print("This is fun.")
+# print('Yay! Printing.')
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this.')
