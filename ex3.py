bf1bf9b3a41e1172bd511e86db5e5ff03d4f4677
chunni <guok.zhao.life@outlook.com>
2022-10-18 16:32:36 +0800

    完成 ex3 数字和数学计算

---
+++ ex3.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+#\File        : ./ex3.py
+#\Author      : chunni
+#\Email       : shachi1758@outlook.com
+#\CreateTime  : 2022-10-18 16:18:50
+#\Version     : 0.1.0
+#\Brief       : 数字和数学计算
+#\Log         :
+
+# 打印
+print("I will now count my chickens:")
+
+# 计算
+print("Hens", 25 + 30 / 6)
+# 计算
+print("Roosters", 100 - 25 * 3 % 4)
+
+# 打印
+print("Now I will count the eggs:")
+
+# 计算
+print(3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6)
+
+# 打印
+print("Is it true that 3 + 2 < 5 - 7?")
+# 比较
+print(3 + 2 < 5 - 7)
+
+# 计算
+print("What is 3 + 2?", 3 + 2)
+# 计算
+print("What is 5 - 7?", 5 - 7)
+# 打印
+print("Oh, that's why it's False.")
+
+# 打印
+print("How about some more.")
+
+# 比较
+print("Is it greater?", 5 > -2)
+# 比较
+print("Is it greater or equal?", 5 >= -2)
+# 比较
+print("Is it less or equal?", 5 <= -2)
