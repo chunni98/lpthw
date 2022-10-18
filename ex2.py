1a8b837cb60785718c8918826c69557c9e0a5152
chunni <guok.zhao.life@outlook.com>
2022-10-18 16:18:41 +0800

    完成 ex2

---
+++ ex2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+#\File        : ex2.py
+#\Author      : chunni
+#\Email       : shachi1758@outlook.com
+#\CreateTime  : 2022-10-18 16:10:14
+#\Version     : 0.1.0
+#\Brief       : 注释 和 # 号
+#\Log         :
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run")
